fe0eb1f6e6a804b4e5d61a8aa5f88403c71324be
sufailbadushat <sufailbadushat@gamil.com>
2022-12-07 23:02:56 +0530

    fist commit

---
+++ src/main/java/com/nest/shoppingcart_backend/ShoppingcartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.shoppingcart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/shoppingcart_backend/ShoppingcartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.shoppingcart_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
